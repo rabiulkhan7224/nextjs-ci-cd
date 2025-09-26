090b5235004af23224e5b4576b84257524fe36a0
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-09-26 17:06:19 +0600

    Initial Next.js setup

---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,8 @@
+import Page from '@/app/page'
+import { render } from '@testing-library/react'
+
+ 
+it('renders homepage unchanged', () => {
+  const { container } = render(<Page />)
+  expect(container).toMatchSnapshot()
+})
